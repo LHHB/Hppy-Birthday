c8c5994fd23d806377d370b6c6c0a7afc7a4b2ab
寒冰 <109894540+LHHB@users.noreply.github.com>
2023-11-30 15:53:51 +0800

    Add files via upload

---
+++ 生日快乐.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int main()
+{
+    system("color e");
+    printf("   Cold ice   \n\n");
+    printf("              \n");
+    printf("       |       \n");
+    printf("  *  ooooo  *  \n");
+    printf("  |  |   |  |  \n");
+    printf("ooooooooooooooo\n");
+    printf("| Éú ÈÕ ¿ì ÀÖ |\n");
+    printf("|_____________|\n\n");
+    printf("HAPPY BIRTHDAY£¡\n\n");
+    return 0;
+}
